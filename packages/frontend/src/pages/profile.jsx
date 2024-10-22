333b62407de0c220e81021b94e85a2ff6ed1843d
StelStelox <andrejt384@gmail.com>
2024-10-22 13:45:46 +0300

    Корректировка стилей

--- packages/frontend/src/pages/profile.jsx
+++ packages/frontend/src/pages/profile.jsx
@@ -81,14 +81,14 @@ export default function Profile() {
   };
 
   return (
-    <div className="flex flex-col md:flex-row items-center md:items-start gap-6 mt-6 p-6 bg-neutral-800 rounded profile">
+    <div className="flex flex-col md:flex-row items-center md:items-start gap-6 mt-6 p-6 bg-neutral-800 rounded max-md:text-center">
       <div>
         <canvas
           ref={skinCanvas}
           className="rounded bg-neutral-700 bg-opacity-20"
         ></canvas>
         <div className="flex items-center justify-center">
-          <span className="text-sm font-medium text-neutral-900 dark:text-neutral-300 mr-2">
+          <span className="text-sm font-medium text-neutral-300 mr-2">
             Плащ
           </span>
           <label className="relative inline-flex items-center cursor-pointer my-2">
@@ -98,8 +98,8 @@ export default function Profile() {
               className="sr-only peer"
               onChange={changeCapeElytra}
             />
-            <div className="w-11 h-6 bg-neutral-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-neutral-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-neutral-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-neutral-600 peer-checked:bg-blue-600"></div>
-            <span className="ms-3 text-sm font-medium text-neutral-900 dark:text-neutral-300">
+            <div className="w-11 h-6 bg-neutral-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-800 rounded-full peer dark:bg-neutral-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-neutral-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-neutral-600 peer-checked:bg-blue-600"></div>
+            <span className="ms-3 text-sm font-medium text-neutral-300">
               Элитры
             </span>
           </label>
@@ -111,7 +111,7 @@ export default function Profile() {
             <tr>
               <td className="font-medium w-24">Тип скина:</td>
               <td className="flex items-center">
-                <span className="text-sm font-medium text-neutral-900 dark:text-neutral-300 mr-2">
+                <span className="text-sm font-medium text-neutral-300 mr-2">
                   Default
                 </span>
                 <label className="relative inline-flex items-center cursor-pointer my-2">
@@ -123,7 +123,7 @@ export default function Profile() {
                     checked={skinType()}
                   />
                   <div className="w-11 h-6 bg-neutral-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-neutral-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-neutral-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-neutral-600 peer-checked:bg-blue-600"></div>
-                  <span className="ms-3 text-sm font-medium text-neutral-900 dark:text-neutral-300">
+                  <span className="ms-3 text-sm font-medium text-neutral-300">
                     Slim
                   </span>
                 </label>
